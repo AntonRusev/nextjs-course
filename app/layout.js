9820b48dd5095c858ccc7e28a12745e51f2cd9c9
AntonRusev <anton_r@abv.bg>
2024-04-19 21:19:37 +0300

    added NavBar, Hero, InfoBox and Footer components

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,33 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 // SEO
 export const metadata = {
-  title: "NextJS tutorial - Property Pulse",
-  description: "Find your dream rental property",
-  keywords: "rental, find rentals, find properties"
+    title: "NextJS tutorial - Property Pulse",
+    description: "Find your dream rental property",
+    keywords: "rental, find rentals, find properties"
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                {/* Navigation Bar */}
+                <NavBar />
+
+                {/* Main content */}
+                <main>
+                    {children}
+                </main>
+
+                {/* Footer */}
+                <Footer />
+            </body>
+        </html>
+    );
 };
--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+
 export default function Home() {
-  return (
-    <section className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Home</h2>
-    </section>
-  );
+    return (
+        <>
+            <Hero />
+            <InfoBoxes />
+        </>
+    );
 }
